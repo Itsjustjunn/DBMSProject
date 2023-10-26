ed22a6cced748ba12a5a0768ddbae19cf1a5aa53
morphen95 <149051649+morphen95@users.noreply.github.com>
2023-10-26 23:43:55 +0800

    gui update
    added filter

--- gui.py
+++ gui.py
@@ -4,27 +4,17 @@ from PIL import Image, ImageTk
 import sqlite3
 import os
 import csv
+import re
 
-##def validate_login():
-##    username = username_entry.get()
-##    password = password_entry.get()
-##
-### Check if the username and password are correct (you can replace this with your own logic)
-##    if username == "sit" and password == "123":
-##        messagebox.showinfo("Login Successful", "Welcome, " + username + "!")
-##        username_label.pack_forget()
-##        username_entry.pack_forget()
-##        password_label.pack_forget()
-##        password_entry.pack_forget()
-##        welcome_label.pack_forget()
-##        # background_image.pack_forgot()
-##        background_label.pack_forget()
-##        login_button.pack_forget()
-##
-##        # Show the search/filter frame
-##        search_frame.pack()
-##    else:
-##        messagebox.showerror("Login Failed", "Invalid username or password")
+max_price_entry = None
+min_lease_entry = None
+max_lease_entry = None
+flat_type_entry = None
+result_text = None
+
+
+with open('ResaleflatpricesbasedonregistrationdatefromJan2017onwards.csv', 'r', newline='') as file:
+    csv_reader = csv.DictReader(file)
 
 def validate_login():
     login_file = open('login.txt', "r")
@@ -54,7 +44,8 @@ def validate_login():
 def display_stats(selected_entry):
     stats_window = tk.Toplevel(root)
     stats_window.title("Detailed Statistics")
-    
+
+
     # Create a text widget to display the selected entry's stats
     helvetica_font = ("Helvetica", 12)
     stats_text = tk.Text(stats_window, wrap=tk.NONE, font=helvetica_font)
@@ -67,11 +58,35 @@ def display_stats(selected_entry):
         
 # Function to perform the search or filter operation
 def search_addresses():
+    global min_price_entry, max_price_entry, min_lease_entry, max_lease_entry, flat_type_entry, result_text
     keyword = search_entry.get()
 
     # Create a new Toplevel window for displaying results
     results_window = tk.Toplevel(root)
     results_window.title("Search Results")
+
+    max_price_label = tk.Label(results_window, text="Max Resale Price:")
+    max_price_label.pack()
+    max_price_entry = tk.Entry(results_window)
+    max_price_entry.pack()
+
+    min_lease_label = tk.Label(results_window, text="Min Remaining Lease (in months):")
+    min_lease_label.pack()
+    min_lease_entry = tk.Entry(results_window)
+    min_lease_entry.pack()
+
+    max_lease_label = tk.Label(results_window, text="Max Remaining Lease (in months):")
+    max_lease_label.pack()
+    max_lease_entry = tk.Entry(results_window)
+    max_lease_entry.pack()
+
+    flat_type_label = tk.Label(results_window, text="Flat Type:")
+    flat_type_label.pack()
+    flat_type_entry = tk.Entry(results_window)
+    flat_type_entry.pack()
+
+    filter_button = tk.Button(results_window, text="Apply Filter", command=apply_filter)
+    filter_button.pack()
 
     # Create a text widget for displaying the results
     helvetica_font = ("Helvetica", 12)
@@ -98,6 +113,58 @@ def search_addresses():
         result_text.tag_bind(f"entry-{i}", "<Button-1>", lambda event, entry=entry: display_stats(entry))
 
 
+def apply_filter():
+    keyword = search_entry.get().lower()
+    max_price_str = max_price_entry.get()
+    min_lease_str = min_lease_entry.get()
+    max_lease_str = max_lease_entry.get()
+    selected_flat_type = flat_type_entry.get().lower()
+
+    max_price = float(max_price_str) if max_price_str else float("inf")
+
+    min_lease_months = None if not min_lease_str else int(min_lease_str)
+    max_lease_months = None if not max_lease_str else int(max_lease_str)
+
+    result_text.config(state=tk.NORMAL)
+    result_text.delete(1.0, tk.END)
+
+    # Create a list to store the details of each matching entry
+    details_list = []
+
+    # Open the CSV file using the 'csv' module
+    with open('ResaleflatpricesbasedonregistrationdatefromJan2017onwards.csv', 'r', newline='') as file:
+        csv_reader = csv.DictReader(file)
+
+        for i, row in enumerate(csv_reader):
+            town = row['town'].lower()
+            resale_price = float(row['resale_price'])
+            remaining_lease = row['remaining_lease']
+            flat_type = row['flat_type'].lower().strip()
+
+            # Extract the numeric parts of the remaining_lease column using regular expressions
+            lease_parts = re.findall(r'\d+', remaining_lease)
+            if lease_parts:
+                remaining_lease_months = int(lease_parts[0])
+            else:
+                remaining_lease_months = 0
+
+            if keyword in town and \
+                    (max_price_str and 0 <= resale_price <= max_price) and \
+                    (min_lease_months is None or min_lease_months <= remaining_lease_months) and \
+                    (max_lease_months is None or remaining_lease_months <= max_lease_months) and \
+                    (selected_flat_type == '' or selected_flat_type == flat_type):
+                result_text.insert(tk.END,
+                                   f"Month: {row['month']}, Town: {row['town']}, Flat Type: {row['flat_type']}, Street Name: {row['street_name']}, Storey Range: {row['storey_range']}, Flat Model: {row['flat_model']}, Lease Commence Date: {row['lease_commence_date']}, Remaining Lease: {remaining_lease}\n")
+                details_list.append(row)
+
+    # Bind a function to each line to display detailed stats when clicked
+    for i, entry in enumerate(details_list):
+        result_text.tag_add(f"entry-{i}", f"{i + 1}.0", f"{i + 1}.end")
+        result_text.tag_bind(f"entry-{i}", "<Button-1>", lambda event, entry=entry: display_stats(entry))
+
+    result_text.config(state=tk.DISABLED)
+
+
 # Create the main application window
 root = tk.Tk()
 root.title("Property Login Page")
@@ -105,13 +172,9 @@ root.title("Property Login Page")
 # set resolution
 root.geometry("1920x1080")
 
-### Set the default resolution to 1920x1080
-##root.wm_attributes('-fullscreen', True)
-##root.attributes('-zoomed', 1)
-
 # Load the background image using Pillow (PIL)
 current_directory = os.getcwd()
-relative_path = "marina.png"
+relative_path = "marina.jpg"
 image_path = os.path.join(current_directory, relative_path)
 image = Image.open(image_path)
 background_image = ImageTk.PhotoImage(image)
@@ -158,8 +221,8 @@ search_button = tk.Button(search_frame, text="Search", command=search_addresses,
 search_button.pack(side="left")
 
 # Create a Listbox to display the search results
-result_listbox = tk.Listbox(search_frame, font=('Arial', 20))
-result_listbox.pack(side="left", fill="both", expand=True)
+# result_listbox = tk.Listbox(search_frame, font=('Arial', 20))
+# result_listbox.pack(side="left", fill="both", expand=True)
 
 # # Create another spacer frame to center-align widgets horizontally
 # center_spacer = tk.Frame(root)
