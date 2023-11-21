c42be2b63d7b965ebdb5bc33ae337b321017fdf4
Itsjustjunn <alphjc2010@gmail.com>
2023-11-22 01:37:50 +0800

    Completed Property.py

--- property.py
+++ property.py
@@ -33,7 +33,7 @@ def getproperty(propertyID):
     try:
         conn = sqlite3.connect(database)
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM Properties WHERE propertyID = ?", (propertyID,))
+        cursor.execute("SELECT * FROM Property WHERE propertyID = ?", (propertyID,))
         property_data = cursor.fetchone()  # Fetch one row
         conn.close()
 
@@ -54,12 +54,65 @@ def getproperty(propertyID):
 
 
 # addProperty(Property property)
+def addproperty(propertyID, propertyName, propertyLocation, propertyPrice):#parameters to be changed
+    try:
+        conn = sqlite3.connect(database)
+        cursor = conn.cursor()
+
+        # Check if the propertyID already exists
+        cursor.execute("SELECT * FROM Property WHERE propertyID = ?", (propertyID,))
+        existing_property = cursor.fetchone()
+
+        if existing_property:
+            print(f"Property with ID {propertyID} already exists.")
+        else:
+            # Insert the new property into the table
+            cursor.execute(
+                "INSERT INTO Property (propertyID, propertyName, propertyLocation, propertyPrice) VALUES (?, ?, ?, ?)",
+                (propertyID, propertyName, propertyLocation, propertyPrice))
+            conn.commit()
+            print(f"Property with ID {propertyID} added successfully.")
+
+        conn.close()
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        conn.rollback()  # Rollback the transaction in case of an error
+        conn.close()
+
+
 # editProperty(string propertyID, Property property)
+def editproperty(propertyID, newPropertyName, newPropertyLocation, newPropertyPrice):#parameters to be changed
+    try:
+        conn = sqlite3.connect(database)
+        cursor = conn.cursor()
+
+        # Check if the propertyID exists
+        cursor.execute("SELECT * FROM Property WHERE propertyID = ?", (propertyID,))
+        existing_property = cursor.fetchone()
+
+        if existing_property:
+            # Update the details of the property
+            cursor.execute("UPDATE Property SET propertyName = ?, propertyLocation = ?, propertyPrice = ? WHERE propertyID = ?",
+                           (newPropertyName, newPropertyLocation, newPropertyPrice, propertyID))
+            conn.commit()
+            print(f"Property with ID {propertyID} updated successfully.")
+        else:
+            print(f"Property with ID {propertyID} does not exist.")
+
+        conn.close()
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        conn.rollback()  # Rollback the transaction in case of an error
+        conn.close()
+
+
 def deleteproperty(propertyID):
     try:
         conn = sqlite3.connect(database)
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM Properties WHERE propertyID = ?", (propertyID,))
+        cursor.execute("DELETE FROM Property WHERE propertyID = ?", (propertyID,))
         conn.commit()
         conn.close()
 
