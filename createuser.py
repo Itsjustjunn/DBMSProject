748f4e4ad6f4ebbbc98b4d9fa82187c75fa527fe
Itsjustjunn <alphjc2010@gmail.com>
2023-11-25 11:03:20 +0800

    Added admin role and one agent account

--- createuser.py
+++ createuser.py
@@ -9,7 +9,8 @@ collection = db['collection_users']
 collection.delete_many({})
 
 doc_list = [
-    {"userid": "sit", "password": "123", "name": "sit", "email": "sit@sit.edu.sg", "phoneno": "88888888", "role": "agent"},
+    {"userid": "sit", "password": "123", "name": "sit", "email": "sit@sit.edu.sg", "phoneno": "88888888", "role": "admin"},
+    {"userid": "agent1", "password": "222", "name": "Dam", "email": "dam@somemail.edu.sg", "phoneno": "80800000", "role": "agent"},
     {"userid": "user1", "password": "456", "name": "Deez", "email": "deez@somemail.com", "phoneno": "80801111", "role": "buyer"},
     {"userid": "user2", "password": "789", "name": "Nutz", "email": "nutz@somemail.com", "phoneno": "80802222", "role": "buyer"},
 ]
